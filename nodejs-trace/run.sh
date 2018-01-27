a2e5d0c1ff04a2224fdf1364f02172e3fe38ad34
kenthua <kenthua@google.com>
2018-01-26 19:51:45 -0500

    added k8s deployments and update / patching

--- nodejs-trace/run.sh
+++ nodejs-trace/run.sh
@@ -15,5 +15,15 @@ gcloud docker -- push gcr.io/$PROJECT_ID/node-app:v1
 gcloud container builds submit --config cloudbuild.yaml .
 ##
 kubectl delete deployment node-web
-kubectl run node-web --image=gcr.io/$PROJECT_ID/node-app:v1 --port 8080 --image-pull-policy Always
-kubectl expose deployment node-web --type=LoadBalancer --port 80 --target-port 8080
+kubectl run node-web --image=gcr.io/$PROJECT_ID/node-app:v1 --port 8080 --image-pull-policy Always --dry-run -o yaml > k8s/deployment.yaml
+kubectl expose deployment node-web --type=LoadBalancer --port 80 --target-port 8080 --dry-run -o yaml > k8s/service.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+
+
+# updating / patching
+gcloud container images add-tag gcr.io/kenthua-testing/node-app:v1 gcr.io/kenthua-testing/node-app:v1.1
+# edit patch.yaml accordingly
+kubectl patch --local -o yaml -f k8s/deployment.yaml -p "$(cat patch.yaml)" > k8s/deployment.yaml
+kubectl apply -f k8s/deployment.yaml
+
