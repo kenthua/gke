10a3599771c4f1d2b3f48a852ff46bf5aa2e0e87
Kent Hua <kenthua@google.com>
2018-10-25 13:45:35 -0700

    deprecated, in favor of https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/tree/master/examples/shared_vpc

--- terraform/private-cluster-shared-vpc/connections.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  #credentials = "${file("service_account.json")}"
-  region = "${var.region}"
-
-  #project = "${var.host_project}"
-
-  # See here more details
-  # https://www.terraform.io/docs/providers/google/index.html
-}
--- terraform/private-cluster/connections.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
-  #credentials = "${file("service_account.json")}"
-  region  = "${var.region}"
-  project = "${var.project}"
-
-  # See here more details
-  # https://www.terraform.io/docs/providers/google/index.html
-}
