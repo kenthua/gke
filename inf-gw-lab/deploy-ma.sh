6bc7c1b5e233e52624a0ee6829e7fccb72a37df3
Kent Hua <kenthua@google.com>
2025-07-22 20:49:22 +0000

    add loggig

--- inf-gw-lab/deploy-ma.sh
+++ inf-gw-lab/deploy-ma.sh
@@ -18,7 +18,9 @@ echo "### Create model armor template"
 gcloud model-armor templates create $MODEL_ARMOR_TEMPLATE_NAME --location $LOCATION \
   --pi-and-jailbreak-filter-settings-enforcement=enabled \
   --pi-and-jailbreak-filter-settings-confidence-level=MEDIUM_AND_ABOVE \
-  --rai-settings-filters='[{ "filterType": "HATE_SPEECH", "confidenceLevel": "MEDIUM_AND_ABOVE" },{ "filterType": "DANGEROUS", "confidenceLevel": "MEDIUM_AND_ABOVE" },{ "filterType": "HARASSMENT", "confidenceLevel": "MEDIUM_AND_ABOVE" },{ "filterType": "SEXUALLY_EXPLICIT", "confidenceLevel": "MEDIUM_AND_ABOVE" }]'
+  --rai-settings-filters='[{ "filterType": "HATE_SPEECH", "confidenceLevel": "MEDIUM_AND_ABOVE" },{ "filterType": "DANGEROUS", "confidenceLevel": "MEDIUM_AND_ABOVE" },{ "filterType": "HARASSMENT", "confidenceLevel": "MEDIUM_AND_ABOVE" },{ "filterType": "SEXUALLY_EXPLICIT", "confidenceLevel": "MEDIUM_AND_ABOVE" }]' \
+  --template-metadata-log-sanitize-operations \
+  --template-metadata-log-operations
 
 echo "### Set permissions for model armor"
 gcloud projects add-iam-policy-binding $PROJECT_ID \
