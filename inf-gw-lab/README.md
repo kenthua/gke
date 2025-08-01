76cf1a23c38608d71615453d4e1213f8dd41b455
Kent Hua <kenthua@google.com>
2025-08-01 18:33:14 +0000

    add script for 1b ft

--- inf-gw-lab/README.md
+++ inf-gw-lab/README.md
@@ -6,7 +6,17 @@ Set of scripts and Kubernetes configuration files to deploy Gemma models through
 Intended to run in the order described:
 
 - `setup.sh` - sets up the cluster and inference gateway prereqs
+
+Choose 1 for 1b:
+
 - `deploy-1b.sh` - deploys gemma 3 1b it resources
+
+or
+
+- `deploy-1b-ft.sh` - deploys gemma 3 1b resources and a lora adapter. You will need to add the lora adapter path to the script.
+
+Continue from here:
+
 - `deploy-4b.sh` - deploys gemma 3 4b it resources
 - `deploy-ma.sh`- configures model armor for the gemama 3 1b it model, which assesses the prompt and responses for Responsible AI and safety filters.
 - `deploy-ca.sh`- configures cloud armor for the gemma models, which requires a HTTP `user-id` header to access resources as a test.
---
+++ inf-gw-lab/deploy-1b-ft.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "### Set env"
+export PROJECT_ID=$(gcloud config get-value project)
+export CLUSTER_NAME=vllm-inference
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export FT_LORA_ADAPTER_PATH=
+export BUCKET=$PROJECT_ID-data
+
+echo "### Get cluster"
+gcloud container clusters get-credentials $CLUSTER_NAME \
+--region $REGION \
+--project $PROJECT_ID
+
+echo "### Create HF secret"
+kubectl create secret generic hf-secret \
+      --from-literal=hf_api_token=$HF_TOKEN \
+      --dry-run=client -o yaml | kubectl apply -f -
+
+echo "### Deploy gemma 3 1b"
+envsubst < gemma-3-1b-ft-lora.yaml.tmpl > gemma-3-1b-ft-lora.yaml
+kubectl apply -f gemma-3-1b-ft-lora.yaml
+
+echo "### Deploy Gradio"
+kubectl apply -f gradio.yaml
+
+echo "### Deploy gateway"
+kubectl apply -f gateway.yaml
+
+echo "### Deploy HTTPRoute for gemma 3 1b"
+kubectl apply -f hr-gemma-3-1b.yaml
+
+echo "### Deploy HTTPRoute for gemma 3 1b ft"
+kubectl apply -f hr-gemma-3-1b-ft.yaml
+
+echo "### Deploy body-based routing"
+helm install bbr \
+  --version v0.3.0 \
+  --set provider.name=gke \
+  --set inferenceGateway.name=vllm-xlb \
+oci://registry.k8s.io/gateway-api-inference-extension/charts/body-based-routing
+
+echo "### Deploy Inference Pool for gemma 3 1b"
+INFERENCE_POOL=vllm-gemma-3-1b
+helm install ${INFERENCE_POOL} \
+  --set inferencePool.modelServers.matchLabels.app=vllm-gemma-3-1b \
+  --set provider.name=gke \
+  --version v0.3.0 \
+  oci://registry.k8s.io/gateway-api-inference-extension/charts/inferencepool
+
+echo "### Deploy Inference Model for gemma 3 1b"
+kubectl apply -f im-gemma-3-1b.yaml
+
+echo "### Deploy Inference Model for gemma 3 1b"
+kubectl apply -f im-gemma-3-1b-ft.yaml
